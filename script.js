2aad54aae0c1dde47bde6fb7c00fa1932d06c1f1
Hardik <naranghardik1903@gmail.com>
2023-05-18 05:06:08 +0530

    done

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let hardik=fetch("https://reqres.in/api/users")
-.then((value)=>{
-    return value.json();
-}).then((data)=>{
-    console.log(data);
-}).catch((err)=>{
-    document.write("Error:404 Server can't Found");
-})
+fetch("https://reqres.in/api/users").then((data)=>{
+    return data.json();
+}).then((Odata)=> {
+   // console.log(Odata.data[0].id);// how to check the value of json
+   let tabledat="";
+   Odata.data.map((values)=>{
+     tabledat  +=`<tr>
+    <th>${values.id}</th>
+    <td>${values.email}</td>
+    <td>${values.first_name}</td>
+    <td>${values.last_name}</td>
+    <td><img src="${values.avatar}" alt=""></td>
+</tr>`;
+   })
+   document.getElementById("table-body").innerHTML=tabledat;
+   
+});
